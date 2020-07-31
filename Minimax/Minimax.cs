1d260818341b5abe3db20c798292ebdde17adcfd
gabymorgi <gabymorgi@gmail.com>
2020-07-31 00:02:47 -0300

    removed count adn now return complete heuristic

--- Minimax/Minimax.cs
+++ Minimax/Minimax.cs
@@ -5,119 +5,52 @@ namespace IA_Utils
 {
     public static class Minimax
     {
-        public static int alphaBetaCuts = 0;
-        public static bool countAlphaBetaCuts = false;
-
-        private struct alphaBetaNode
+        public static Heuristic GetBestNode(MinimaxNode node, int depth)
         {
-            public MinimaxNode node;
-            public int value;
-        }
-        public static MinimaxNode GetBestNode(MinimaxNode node, int depth)
-        {
-            alphaBetaCuts = 0;
             if (node == null) return null;
-            alphaBetaNode result = alphaBeta(node, depth, int.MinValue, int.MaxValue, true);
-            return result.node;
+            return alphaBeta(node, depth, int.MinValue, int.MaxValue, true);
         }
 
-        private static alphaBetaNode alphaBeta(MinimaxNode node, int depth, int alpha, int beta, bool maximizingPlayer)
+        private static Heuristic alphaBeta(MinimaxNode node, int depth, int alpha, int beta, bool maximizingPlayer)
         {
-            if (node.IsTerminal() || depth == 0)
+            if (node.IsTerminal() || depth < 1)
             {
-                return new alphaBetaNode { node = node, value = node.GetHeuristic() };
+                return new Heuristic { node = node, value = node.GetHeuristic() };
             }
             List<MinimaxNode> subNodes = node.GetSubNodes();
             if (maximizingPlayer)
             {
-                alphaBetaNode abNode = new alphaBetaNode { value = int.MinValue };
-                alphaBetaNode newAbNode;
+                Heuristic abNode = new Heuristic { value = int.MinValue };
+                Heuristic newAbNode;
                 for (int i = 0; i < subNodes.Count; i++)
                 {
                     newAbNode = alphaBeta(subNodes[i], depth - 1, alpha, beta, false);
-                    if (newAbNode.value > abNode.value) abNode = new alphaBetaNode { node = subNodes[i], value = newAbNode.value };
+                    if (newAbNode.value > abNode.value) abNode = new Heuristic { node = subNodes[i], value = newAbNode.value };
                     if (abNode.value > alpha) alpha = abNode.value;
-                    if (alpha >= beta)
-                    {
-                        if (countAlphaBetaCuts)
-                        {
-                            List<MinimaxNode> nodesBeingCut = new List<MinimaxNode>();
-                            if (depth == 1)
-                            {
-                                alphaBetaCuts += subNodes.Count - i;
-                            }
-                            else
-                            {
-                                for (int j = i + 1; j > subNodes.Count; j++)
-                                {
-                                    nodesBeingCut.AddRange(subNodes[j].GetSubNodes());
-                                }
-                                int depthCopy = depth - 1;
-                                alphaBetaCuts += nodesBeingCut.Count;
-                                while (depthCopy > 0)
-                                {
-                                    List<MinimaxNode> nodesBeingCutCopy = nodesBeingCut;
-                                    nodesBeingCut = new List<MinimaxNode>();
-                                    for (int j = 0; j > nodesBeingCutCopy.Count; j++)
-                                    {
-                                        nodesBeingCut.AddRange(nodesBeingCutCopy[j].GetSubNodes());
-                                    }
-                                    depthCopy--;
-                                    alphaBetaCuts += nodesBeingCut.Count;
-                                }
-                            }
-                        }
-                        break;
-                    }
+                    if (alpha >= beta) break;
                 }
                 return abNode;
             }
             else
             {
-                alphaBetaNode abNode = new alphaBetaNode { value = int.MaxValue };
-                alphaBetaNode newAbNode;
+                Heuristic abNode = new Heuristic { value = int.MaxValue };
+                Heuristic newAbNode;
                 for (int i = 0; i < subNodes.Count; i++)
                 {
                     newAbNode = alphaBeta(subNodes[i], depth - 1, alpha, beta, true);
-                    if (newAbNode.value < abNode.value) abNode = new alphaBetaNode { node = subNodes[i], value = newAbNode.value };
+                    if (newAbNode.value < abNode.value) abNode = new Heuristic { node = subNodes[i], value = newAbNode.value };
                     if (abNode.value < beta) beta = abNode.value;
-                    if (beta <= alpha)
-                    {
-                        if (countAlphaBetaCuts)
-                        {
-                            List<MinimaxNode> nodesBeingCut = new List<MinimaxNode>();
-                            if (depth == 1)
-                            {
-                                alphaBetaCuts += subNodes.Count - i;
-                            }
-                            else
-                            {
-                                alphaBetaCuts += subNodes.Count - i;
-                                for (int j = i + 1; j > subNodes.Count; j++)
-                                {
-                                    nodesBeingCut.AddRange(subNodes[j].GetSubNodes());
-                                }
-                                int depthCopy = depth - 1;
-                                alphaBetaCuts += nodesBeingCut.Count;
-                                while (depthCopy > 0)
-                                {
-                                    List<MinimaxNode> nodesBeingCutCopy = nodesBeingCut;
-                                    nodesBeingCut = new List<MinimaxNode>();
-                                    for (int j = 0; j > nodesBeingCutCopy.Count; j++)
-                                    {
-                                        nodesBeingCut.AddRange(nodesBeingCutCopy[j].GetSubNodes());
-                                    }
-                                    depthCopy--;
-                                    alphaBetaCuts += nodesBeingCut.Count;
-                                }
-                            }
-                        }
-                        break;
-                    }
+                    if (beta <= alpha) break;
                 }
                 return abNode;
             }
         }
+    }
+
+    public class Heuristic
+    {
+        public MinimaxNode node;
+        public int value;
     }
 
     public interface MinimaxNode
